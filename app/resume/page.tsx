2c7682fff64ec733989b81950afca27ddabfb45e
Eric <anspring05eric@gmail.com>
2025-09-22 23:52:31 -0400

    Fixed redirects to work with static export + updated resume
    Redirects in the config don't work in static exports, redirect using a page instead

---
+++ app/resume/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from 'next/navigation';
+
+export default async function Resume() {
+    redirect('/Eric_Chu_Resume_Fall_2025.pdf');
+}
--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
-  async redirects(){
-    return[
-      {
-        source: '/resume',
-        destination: '/Eric_Chu_June_2025_Resume.pdf',
-        permanent: true,
-      },
-    ]
-  },
 };
 
 export default nextConfig;
